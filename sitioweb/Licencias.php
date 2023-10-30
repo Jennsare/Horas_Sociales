45542230893d5058a46c8522633fe58a226e4cd3
Jennsare <148657136+Jennsare@users.noreply.github.com>
2023-10-30 16:50:02 -0600

    Add files via upload

---
+++ sitioweb/Licencias.php
@@ -0,0 +1,51 @@
+<?php include ("template/cabecera.php"); ?>
+
+<div class="col-md-3">
+
+<div class="card">
+    <div class="card-body">
+        <h4 class="card-title">Licencia 1</h4>
+        <p class="card-text">Des</p>
+    </div>
+    </div>
+</div>
+
+<div class="col-md-3">
+
+<div class="card">
+    <div class="card-body">
+        <h4 class="card-title">Licencia 1</h4>
+        <p class="card-text">Des</p>
+    </div>
+    </div>
+</div>
+
+
+<div class="col-md-3">
+
+<div class="card">
+    <div class="card-body">
+        <h4 class="card-title">Licencia 1</h4>
+        <p class="card-text">Des</p>
+    </div>
+    </div>
+</div>
+
+
+<div class="col-md-3">
+
+<div class="card">
+    <div class="card-body">
+        <h4 class="card-title">Licencia 1</h4>
+        <p class="card-text">Des</p>
+    </div>
+    </div>
+</div>
+
+
+
+
+
+
+
+<?php include ("template/pie.php"); ?>
---
+++ sitioweb/template/cabecera.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+ <html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Administrador de Licencias</title> 
+
+    
+    <Link rel="stylesheet" href="./css/bootstrap.min.css"/> 
+ </head>
+ </body>
+
+ 
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <ul class="nav navbar-nav">
+
+        <li class="nav-item active">
+            <a class="nav-link" href="#">Inicio de Sesion </a>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link" href="#">Administrador</a>
+        </li>
+
+    </ul>
+</nav>
+
+<div class="container">
+    <br/>
+    <div class= "row">
